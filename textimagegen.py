dad16c37e5856d6047d9f0274258c37eb7f60881
Some Stupid Idiot <raspberryepicly@gmail.com>
2022-09-18 16:06:35 +0800

    migrated to discord.py 2.0

--- textimagegen.py
+++ textimagegen.py
@@ -2,9 +2,10 @@ from PIL import Image, ImageDraw,ImageFont
 from discord.ext import commands
 import discord
 import random
+import io
 
 
-bot = commands.Bot(command_prefix=['?'])
+bot = commands.Bot(command_prefix=['?'],intents=discord.Intents.all())
 
 BotToken='Put your bot token here'
 
@@ -17,25 +18,26 @@ async def image(ctx,*,text):
 	font=ImageFont.truetype('roboto.ttf',20)
 	
 	#randomizes colors value but ''a" is set to 255
-	r,g,b,a=random.randint(0,255),random.randint(0,255),random.randint(0,255),255
+	rgb=random.randint(0,255),random.randint(0,255),random.randint(0,255),255
 	
 	#makes new image to draw
-	temp= Image.new('RGBA',(150,100),(r,g,b,a))
+	temp= Image.new('RGBA',(150,100),rgb)
 	
 	#creates a drawing context
 	tempd=ImageDraw.Draw(temp)
 	
 	#randomizes the colors for text's
-	r,g,b,a=random.randint(0,255),random.randint(0,255),random.randint(0,255),255
+	rgb=random.randint(0,255),random.randint(0,255),random.randint(0,255),255
 	
 	#applying the text and its font
-	tempd.text((5,5),text,font=font,fill=(r,g,b,a))
+	tempd.text((5,5),text,font=font,fill=rgb)
 	
-	#saves it
-	temp.save('image.png')
+	#saves it to a buffer 
+	buffer=io.BytesIO()
+	temp.save(buffer,"PNG")	
 	
 	#sends it to the chat
-	await ctx.send(file=discord.File('image.png'))
+	await ctx.send(file=discord.File(buffer,filename='image.png'))
 
 bot.run(BotToken)
 
